6d3fee0b93c19f0f46bd248efd11104e81b61e5a
Sumapraja <irfansumapraja@gmail.com>
2019-07-02 11:33:00 +0700

    changing example to my preferences and adding exim functions

--- index.js
+++ index.js
@@ -1,21 +1,72 @@
-class Car {
-    constructor(owner = 'Unknown Owner', name = 'Unknown Name') {
-      this.owner = owner
-      this.name = name
+class AgentsTeam {
+    constructor({name, initialTeam, lastTeam}) {
+        this.name = name
+        this.initialTeam = initialTeam
+        this.lastTeam = lastTeam
     }
-  
-    drive() {
-      console.log(
-        `The ${this.name} car owned by ${this.owner} drives on the road`
-      )
+    move() {
+        console.log(
+            `${this.name} has decided to move out from ${this.initialTeam} to ${this.lastTeam}`
+        )
     }
-  
-    getOwner() {
-      return this.owner
+    getName() {
+        return this.name
     }
-  
-    getOwner() {
-      return this.name
+    getlastTeam() {
+        return this.lastTeam
     }
-  }
-  
+    getinitialTeam() {
+        return this.initialTeam
+    }
+}
+const freeAgentsTeam = new AgentsTeam({
+    name: 'KD',
+    initialTeam: 'Warriors',
+    lastTeam: 'Nets'
+})
+console.log(freeAgentsTeam)
+freeAgentsTeam.move()
+console.log(freeAgentsTeam.getinitialTeam())
+console.log(freeAgentsTeam.getlastTeam())
+
+class AgentsContract extends AgentsTeam {
+    constructor({ name, initialTeam, lastTeam, year, contract }) {
+        super({name, initialTeam, lastTeam})
+        this.year = year
+        this.contract = contract
+    }
+    contracts() {
+        console.log(`${this.name} has decided to move out from ${this.initialTeam} to ${this.lastTeam} for a ${this.year} year(s) ${this.contract} contracts`);
+
+    }
+    getYear() {
+        return this.year
+    }
+
+    getContract() {
+        const formattedcontract = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 0
+        }).format(this.contract)
+
+        return formattedcontract
+    }
+}
+
+const freeAgentsContract = new AgentsContract({
+    name: 'KD',
+    initialTeam: 'Warriors',    
+    lastTeam: 'Nets',
+    year: 4,
+    contract: 141000000
+})
+
+console.log(freeAgentsContract);
+freeAgentsContract.contracts();
+console.log(freeAgentsContract.getYear());
+console.log(freeAgentsContract.getContract());
+
+
+//---------------exim--------------
+
